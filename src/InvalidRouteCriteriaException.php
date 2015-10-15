48bdae6b72b5968e9bc60a845389163248320484
Matt Ferris <matt@bueller.ca>
2015-10-15 12:04:33 -0700

    Added InvalidRouteCriteriaException

---
+++ src/InvalidRouteCriteriaException.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * HttpRouting - An HTTP routing dispatcher
+ * www.bueller.ca/http-routing
+ *
+ * InvalidRouteCriteriaException.php
+ * @copyright Copyright (c) 2015
+ * @author Matt Ferris <matt@bueller.ca>
+ *
+ * Licensed under BSD 2-clause license
+ * www.bueller.ca/http-routing/license
+ */
+
+namespace MattFerris\HttpRouting;
+
+class InvalidRouteCriteriaException extends \Exception
+{
+}
+
